a93aa1cee252f15a40a64d6638ac9c44759424f3
3dpython <igloo_info@bk.ru>
2023-04-28 10:17:35 +0300

    Updated files to ignore

--- functions.py
+++
@@ -1,9 +0,0 @@
-def form_year(year: int) -> str:
-    f"Функция, которая генерирует правильную форму слова \'год\'"
-    if year % 100 == 1:
-        return 'год'
-    elif (year % 100) in range(2, 5):
-        return 'года'
-    else:
-        return 'лет'
-
