46ef01fdc3cc26488a28ab973941a2450647dde2
3dpython <igloo_info@bk.ru>
2023-04-28 10:07:32 +0300

    Removed this file from git

--- functions.py
+++ functions.py
@@ -1,43 +1,4 @@
-import datetime as dt
-
-foundation_date = dt.datetime(
-    year=1920,
-    month=1,
-    day=1
-)
-
-
-def years_ago(years, from_date=None):
-    """
-    Рандомный рабочий код с StackOverflow
-    Учитывает високосный год
-    """
-    if from_date is None:
-        from_date = dt.datetime.now()
-    try:
-        return from_date.replace(year=from_date.year - years)
-    except ValueError:
-        # Must be 2/29!
-        assert from_date.month == 2 and from_date.day == 29 # can be removed
-        return from_date.replace(month=2, day=28,
-                                 year=from_date.year-years)
-
-
-def num_years(begin=foundation_date, end=None) -> int:
-    """
-    Рандомный рабочий код с StackOverflow
-    Учитывает високосный год
-    """
-    if end is None:
-        end = dt.datetime.now()
-    years_passed = int((end - begin).days / 365.2425)
-    if begin > years_ago(years_passed, end):
-        return years_passed - 1
-    else:
-        return years_passed
-
-
-def year_form(year: int) -> str:
+def form_year(year: int) -> str:
     f"Функция, которая генерирует правильную форму слова \'год\'"
     if year % 100 == 1:
         return 'год'
